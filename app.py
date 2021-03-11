ecebf010f08fcb9f363d79eeb2242db8bee9260c
Erika Grosse Hokamp <erika.grossehokamp@gmail.com>
2021-03-11 11:49:40 +0100

    final version streamlit app

--- app.py
+++ app.py
@@ -2,29 +2,51 @@ import streamlit as st
 import time
 import requests
 
+# website configuration
+st.set_page_config(
+    page_title='AI-rtsie',
+    page_icon='🎨🤖',
+    layout ='wide',
+    initial_sidebar_state='expanded')
+
+# AI-rtsie API URL
 url = 'https://airtsie-ilga65pf7a-ew.a.run.app/image'
 
-st.markdown('''
+st.markdown('''# AI-rtsie''')
 
-# **AI-rtsie**,
-the artsy storyteller
+st.markdown(
+    '''the ✨artsy✨ storyteller'''
+)
+st.markdown('')
+st.markdown('')
 
-Hey there!
-I am an artifical intelligence 🤖 that turns your text into ✨*artsy*✨ images.
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('''
+
+Hey there!\n
+I am an artifical intelligence that turns your text into ✨*artsy*✨ images.
 
 ''')
 
-text = st.text_input('Tell me something interesting:')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
 
-if st.button('Submit'):
-    with st.spinner('I am generating your images...'):
+text = st.sidebar.text_area('Tell me something interesting:')
 
+if st.sidebar.button('Submit'):
+    with st.spinner("🤖 Let's see what I come up with..."):
         # make a request to OUR api
         params = {'text': text}
         result = requests.get(url, params=params).json()
 
     # debugging check
-    print(result)
+    # print(result)
 
     # check it response is empty
     if result['OK'] == 0:
@@ -47,7 +69,7 @@ if st.button('Submit'):
                     with cols13[i]:
 
                         st.image(result['im_urls'][i])
-                        with st.beta_expander("See original picture"):
+                        with st.beta_expander("See original pictures"):
                             if result['content'][2][i] == '':
                                 cont_attr = f"{result['content'][1][i]} / [Pixabay](https://pixabay.com/)"
                             else:
@@ -63,13 +85,15 @@ if st.button('Submit'):
 
             else:
                 cols13 = st.beta_columns(3)
+                # empty space to separate the rows a bit more
+                st.markdown('')
                 cols46 = st.beta_columns(3)
 
                 for i in range(3):
                     with cols13[i]:
 
                         st.image(result['im_urls'][i])
-                        with st.beta_expander("See original picture"):
+                        with st.beta_expander("See original pictures"):
                             if result['content'][2][i] == '':
                                 cont_attr = f"{result['content'][1][i]} / [Pixabay](https://pixabay.com/)"
                             else:
@@ -81,13 +105,16 @@ if st.button('Submit'):
                             style_attr = f"[{result['style'][1][i]}]({result['style'][2][i]}) / [Unsplash](https://unsplash.com/)"
                             st.image(result['style'][0][i], caption='style image')
                             st.markdown(style_attr)
+                    time.sleep(2)
 
-                # not correct yet
+                # empty space to separate the rows a bit more
+                st.markdown('')
+
                 for i in range(3, n_images):
                     with cols46[i-3]:
 
                         st.image(result['im_urls'][i])
-                        with st.beta_expander("See original picture"):
+                        with st.beta_expander("See original pictures"):
                             if result['content'][2][i] == '':
                                 cont_attr = f"{result['content'][1][i]} / [Pixabay](https://pixabay.com/)"
                             else:
@@ -99,9 +126,18 @@ if st.button('Submit'):
                             style_attr = f"[{result['style'][1][i]}]({result['style'][2][i]}) / [Unsplash](https://unsplash.com/)"
                             st.image(result['style'][0][i], caption='style image')
                             st.markdown(style_attr)
+                    time.sleep(2)
 
-st.markdown(
-
-    '*Powered by Neural Style Transfer library of TensorFlow with “magenta/arbitrary-image-stylization-v1-256”*'
-
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown('')
+st.sidebar.markdown(
+    "<h1 style='text-align: center; color: grey; font-size: 10px; font-family: arial;'>Powered by Neural Style Transfer library of TensorFlow with magenta/arbitrary-image-stylization-v1-256</h1>", unsafe_allow_html=True
     )
